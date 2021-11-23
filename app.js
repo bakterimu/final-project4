2e55d246804b8ef284b8166666d91dd2e1154de2
izharmayrnd <mayrandai12@gmail.com>
2021-11-23 14:20:47 +0700

    commenting routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes.js');
-const photoRoutes = require('./routes/photoRoutes.js');
-const commentRoutes = require('./routes/commentRoutes');
-const socialMediaRoutes = require('./routes/socialMediaRoutes.js');
-const authRoutes = require('./routes/authRoutes');
+// const photoRoutes = require('./routes/photoRoutes.js');
+// const commentRoutes = require('./routes/commentRoutes');
+// const socialMediaRoutes = require('./routes/socialMediaRoutes.js');
+// const authRoutes = require('./routes/authRoutes');
 const port = 8000;
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(userRoutes);
 app.listen(port, () => {
   console.log('Listening on port: ', port);
 });
