357e1e0edf649b844b1dbad5910589444019d22b
wahyutaqdirulaziz <wahyudipe6@gmail.com>
2021-12-17 18:40:06 +0800

    fix

--- routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const commentController = require('./../controllers/commentControllers.js');
-const route = require('express').Router();
-
-route.post('/comments', commentController.createComment);
-route.get('/comments', commentController.getComment);
-route.put('/comments/:comment_id', commentController.editcomment);
-route.delete('/comments/:comment_id', commentController.deleteComment);
-
-module.exports = route;
