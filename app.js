555fbb0ef9b525504f3c6055928f645745b65d70
Izhar Mayranda <mayrandai1290@gmail.com>
2022-02-19 05:43:39 +0700

    memisahkan server dan app js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require('./routes')(app);
 
-app.listen(port, () => {
-    console.log('Listening on port: ', port);
-});
+
+module.exports = app;
