f937632008ad9a1e4b2344ddbf159e38f9ce82d7
Izhar Mayranda <mayrandai1290@gmail.com>
2022-02-19 05:45:19 +0700

    delete console log

--- midleware/authtoken.js
+++ midleware/authtoken.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['token']
-    console.log(authHeader)
     if (authHeader == null) return res.status(401).send({
         message: 'Unauthorizet',
         status: false
